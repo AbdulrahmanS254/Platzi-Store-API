237a372ed4f626518bcad731fac3c96eeb6dca3b
Abdulrahman Shawky <hawk18922@gmail.com>
2025-09-18 09:46:24 +0300

    filtering product with categories done/max&min price remains

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,8 @@
 // Fetching the products from the API
-// page limit
+// page limit of displaying products
 const limit = 9;
 
-async function getProducts(pageNum = 1) {
+async function getProducts(pageNum = 1, catId) {
     try {
         const offset = (pageNum - 1) * limit;
         const response = await fetch(
@@ -15,15 +15,13 @@ async function getProducts(pageNum = 1) {
 
         const data = await response.json();
         if (data.length < 9) {
-            // prosContainer.innerHTML = "<p>No more products</p>";
             next.disabled = true;
-            // return;
         } else {
             next.disabled = false;
         }
         return data;
     } catch (err) {
-        console.log("Error", err);
+        console.log("Getting products Error", err);
     }
 }
 
@@ -81,32 +79,69 @@ const createCard = (
     prosContainer.appendChild(cardBody);
 };
 
-// === HANDLING THE PAGINATION === //
-
-// Pagination buttons
-const prev = document.getElementById("prevPage");
-const pageInfo = document.getElementById("pageInfo");
-const next = document.getElementById("nextPage");
-
-let currentPage = 1;
-
-prev.addEventListener("click", (ev) => {
-    if (currentPage > 1) {
-        currentPage--;
-        pageInfo.textContent = `Page ${currentPage}`;
-        renderProducts(currentPage);
-    }
-});
-
-next.addEventListener("click", (ev) => {
-    currentPage++;
-    pageInfo.textContent = `Page ${currentPage}`;
-    renderProducts(currentPage);
-});
-
 const renderProducts = (page) => {
     prosContainer.innerHTML = "";
-    getProducts(page).then((data) =>
+        getProducts(page).then((data) =>
+            data.forEach((product) => {
+                createCard(
+                    product.id,
+                    product.images?.[0],
+                    product.slug,
+                    product.title,
+                    product.description,
+                    product.category.name,
+                    product.price
+                );
+            })
+        );
+};
+
+// === Filtering with categories ===
+// Getting categories from the categories api
+async function getCategories() {
+    try {
+        let response = await fetch(
+            `https://api.escuelajs.co/api/v1/categories`
+        );
+        if (!response.ok) {
+            // study note: response.ok returns boolean
+            throw new Error("Getting categories Error" + response.status);
+        }
+        let data = await response.json();
+        return data;
+    } catch (err) {
+        console.log("Error", err);
+    }
+}
+
+// Getting the products by category
+
+async function getProductsByCategory(pageNum = 1, catId) {
+    try {
+        const offset = (pageNum - 1) * limit;
+        let res = await fetch(
+            `https://api.escuelajs.co/api/v1/categories/${catId}/products?offset=${offset}&limit=${limit}`
+        );
+        if (!res.ok) {
+            throw new Error("Getting products by category Error", res.status);
+        }
+        let data = await res.json();
+        if (data.length < 9) {
+            next.disabled = true;
+        } else {
+            next.disabled = false;
+        }
+        return data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// render products by category function
+
+const renderProductsByCategory = (page, cat) => {
+    prosContainer.innerHTML = "";
+    getProductsByCategory(page, cat).then((data) =>
         data.forEach((product) => {
             createCard(
                 product.id,
@@ -121,24 +156,6 @@ const renderProducts = (page) => {
     );
 };
 
-// === Filtering with categories ===
-
-// Getting categories from the categories api
-async function getCategories(catId) {
-    try {
-        let response = await fetch(
-            `https://api.escuelajs.co/api/v1/categories`
-        );
-        if (!response.ok) {
-            throw new Error("Status " + response.status);
-        }
-        let data = await response.json();
-        return data;
-    } catch (err) {
-        console.log("Error", err);
-    }
-}
-
 // the select menu and the filtering logic
 const selectCategory = document.getElementById("category");
 
@@ -150,10 +167,8 @@ const resetFilterBtn = document.getElementById("resetFilters");
 
 const createSelect = (category, id) => {
     const option = document.createElement("option");
-    option.value = category;
+    option.value = id;
     option.textContent = category;
-    option.classList.add('option')
-    option.setAttribute("catId", id);
     selectCategory.appendChild(option);
 };
 
@@ -163,16 +178,62 @@ getCategories().then((data) => {
     });
 });
 
+let choosedCategory = null;
+let productsAreFiltered = false;
+
 selectCategory.addEventListener("change", (e) => {
-    let categoryOption = document.querySelectorAll('option');
+    choosedCategory = selectCategory.value;
+
+    if (choosedCategory === "all") {
+        productsAreFiltered = false;
+        renderProducts(currentPage);
+    } else {
+        productsAreFiltered = true;
+        renderProductsByCategory(currentPage, choosedCategory);
+    }
 });
 
 // Resetting the filter and rerendering the page
 resetFilterBtn.addEventListener("click", (ev) => {
     selectCategory.value = "all";
+    productsAreFiltered = false;
+    choosedCategory = null;
     minPriceInput.value = "";
     maxPriceInput.value = "";
     renderProducts(currentPage);
 });
 
+// === HANDLING THE PAGINATION === //
+
+// Pagination buttons
+const prev = document.getElementById("prevPage");
+const pageInfo = document.getElementById("pageInfo");
+const next = document.getElementById("nextPage");
+
+let currentPage = 1;
+
+prev.addEventListener("click", (ev) => {
+    if (currentPage > 1) {
+        currentPage--;
+        pageInfo.textContent = `Page ${currentPage}`;
+
+        if (productsAreFiltered) {
+            renderProductsByCategory(currentPage, choosedCategory);
+        } else {
+            renderProducts(currentPage);
+        }
+    }
+});
+
+next.addEventListener("click", (ev) => {
+    currentPage++;
+    pageInfo.textContent = `Page ${currentPage}`;
+    if (productsAreFiltered) {
+        renderProductsByCategory(currentPage, choosedCategory);
+    } else {
+        renderProducts(currentPage);
+    }
+});
+
+// === INITIAL RENDER ===
 renderProducts(currentPage);
